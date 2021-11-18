b783b2d09cc652ef33f69c6c82b8b57fd32a21e2
Kamil1812 <kamilniewiadomski9@gmail.com>
2021-11-18 12:59:01 +0100

    Zadanie napisane ale niesprawdzone

--- app/src/main/java/com/example/myapplication_cwiczenie/MainActivity.kt
+++ app/src/main/java/com/example/myapplication_cwiczenie/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.myapplication_cwiczenie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var bString = findViewById<Button>(R.id.btn1);
+        var bRuntime = findViewById<Button>(R.id.btn2);
+
+        var tString = findViewById<TextView>(R.id.Tekst);
+        var tRunTime = findViewById<TextView>(R.id.Runtime);
+
+        var Runtime = "Kamil \n Niewiadomski";
+
+        bRuntime.setOnClickListener()
+        {
+            tRunTime.text = Runtime;
+        }
+
+        bString.setOnClickListener()
+        {
+            tString.text = getResources().getString(R.string.my_name);
+        }
+
     }
 }
